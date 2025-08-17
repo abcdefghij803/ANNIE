15a1d7d14080736d88aff0254836d1ecf85f3927
abcdefghij803 <analtamash1@gmail.com>
2025-08-17 21:28:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+zOo21P9qDW4wZjk1")
 
 API_URL = getenv("API_URL", 'https://p01--api--gmcxsnhyb2tw.code.run') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://p01--api--gmcxsnhyb2tw.code.run')
-API_KEY = getenv("API_KEY", '') # youtube song api key
+API_KEY = getenv("API_KEY", 'Moon@1234') # youtube song api key
 
 # ❖ SERVER LIMITS AND CONFIGURATIONS - THESE CAN BE SET BASED ON YOUR SERVER CONFIGURATIONS 
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
