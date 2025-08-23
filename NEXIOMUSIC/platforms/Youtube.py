320c7ab66468e050f5ec23f87525d58db5f488ed
abcdefghij803 <analtamash1@gmail.com>
2025-08-23 12:43:01 +0530

    Update Youtube.py

--- NEXIOMUSIC/platforms/Youtube.py
+++ NEXIOMUSIC/platforms/Youtube.py
@@ -6,16 +6,17 @@ from typing import Union
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from NEXIOMUSIC.utils.database import is_on_off
 from NEXIOMUSIC.utils.formatters import time_to_seconds
 
+# Download folder
 DOWNLOADS = "downloads"
 os.makedirs(DOWNLOADS, exist_ok=True)
 
 
+# ------------------- Direct Audio Downloader -------------------
 async def download_song(link: str):
     """
-    Directly download best audio using yt_dlp and return file path.
+    Download best audio using yt_dlp and return file path.
     """
     loop = asyncio.get_running_loop()
 
@@ -27,6 +28,7 @@ async def download_song(link: str):
             "no_warnings": True,
             "geo_bypass": True,
             "nocheckcertificate": True,
+            "cachedir": False,
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(link, download=True)
@@ -35,6 +37,7 @@ async def download_song(link: str):
     return await loop.run_in_executor(None, _download)
 
 
+# ------------------- YouTube API Wrapper -------------------
 class YouTubeAPI:
     def __init__(self):
         self.base = "https://www.youtube.com/watch?v="
@@ -50,12 +53,13 @@ class YouTubeAPI:
         messages = [message_1]
         if message_1.reply_to_message:
             messages.append(message_1.reply_to_message)
+
         for message in messages:
             if message.entities:
                 for entity in message.entities:
                     if entity.type == MessageEntityType.URL:
                         text = message.text or message.caption
-                        return text[entity.offset : entity.offset + entity.length]
+                        return text[entity.offset: entity.offset + entity.length]
             elif message.caption_entities:
                 for entity in message.caption_entities:
                     if entity.type == MessageEntityType.TEXT_LINK:
@@ -67,51 +71,78 @@ class YouTubeAPI:
             link = self.base + link
         if "&" in link:
             link = link.split("&")[0]
+
         results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            title = result["title"]
-            duration_min = result["duration"]
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-            vidid = result["id"]
-            duration_sec = int(time_to_seconds(duration_min)) if duration_min else 0
+        result = (await results.next())["result"][0]
+
+        title = result["title"]
+        duration_min = result["duration"]
+        thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        vidid = result["id"]
+        duration_sec = int(time_to_seconds(duration_min)) if duration_min else 0
+
         return title, duration_min, duration_sec, thumbnail, vidid
 
     async def title(self, link: str, videoid: Union[bool, str] = None):
         if videoid:
             link = self.base + link
         results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            return result["title"]
+        return (await results.next())["result"][0]["title"]
 
     async def duration(self, link: str, videoid: Union[bool, str] = None):
         if videoid:
             link = self.base + link
         results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            return result["duration"]
+        return (await results.next())["result"][0]["duration"]
 
     async def thumbnail(self, link: str, videoid: Union[bool, str] = None):
         if videoid:
             link = self.base + link
         results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            return result["thumbnails"][0]["url"].split("?")[0]
+        return (await results.next())["result"][0]["thumbnails"][0]["url"].split("?")[0]
 
     async def track(self, link: str, videoid: Union[bool, str] = None):
         if videoid:
             link = self.base + link
         results = VideosSearch(link, limit=1)
-        for result in (await results.next())["result"]:
-            return {
-                "title": result["title"],
-                "link": result["link"],
-                "vidid": result["id"],
-                "duration_min": result["duration"],
-                "thumb": result["thumbnails"][0]["url"].split("?")[0],
-            }, result["id"]
+        result = (await results.next())["result"][0]
+        return {
+            "title": result["title"],
+            "link": result["link"],
+            "vidid": result["id"],
+            "duration_min": result["duration"],
+            "thumb": result["thumbnails"][0]["url"].split("?")[0],
+        }, result["id"]
 
     async def download(
         self,
+        link: str,
+        mystic=None,
+        video: Union[bool, str] = None,
+        songaudio: Union[bool, str] = None,
+    ):
+        loop = asyncio.get_running_loop()
+
+        if video:
+            def _video_dl():
+                ydl_opts = {
+                    "format": "bestvideo[height<=720]+bestaudio/best",
+                    "outtmpl": os.path.join(DOWNLOADS, "%(id)s.%(ext)s"),
+                    "quiet": True,
+                    "geo_bypass": True,
+                    "nocheckcertificate": True,
+                    "cachedir": False,
+                }
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    info = ydl.extract_info(link, download=True)
+                    return os.path.abspath(ydl.prepare_filename(info))
+
+            filepath = await loop.run_in_executor(None, _video_dl)
+            return filepath, True
+
+        else:
+            filepath = await download_song(link)
+            return filepath, True        self,
         link: str,
         mystic=None,
         video: Union[bool, str] = None,
