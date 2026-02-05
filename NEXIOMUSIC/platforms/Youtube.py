9c92a45590d884ab095e4cc98ab3682f781c8a44
abcdefghij803 <analtamash1@gmail.com>
2026-02-05 15:32:08 +0530

    Update Youtube.py

--- NEXIOMUSIC/platforms/Youtube.py
+++ NEXIOMUSIC/platforms/Youtube.py
@@ -5,7 +5,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 from NEXIOMUSIC.utils.formatters import time_to_seconds
 import aiohttp
 from NEXIOMUSIC import LOGGER
